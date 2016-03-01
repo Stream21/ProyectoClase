1b2c6c47adab387b8f408959bcee939035c38569
Antonio <Antonio@Stream>
2016-03-01 15:11:30 +0000

    Cambio en La clase Inicio en la cual inicializo una variable llamada pruebaEts y al musetr por pantalla

--- ProyectoFinal/Vistas/Inicio.java
+++ ProyectoFinal/Vistas/Inicio.java
@@ -2,9 +2,9 @@
 public class Inicio {
 
 	public static void main(String[] args) {
-		
+		int pruebaEts = 13;
 		VistaPrincipal vp = new VistaPrincipal();
-		
+		System.out.println(pruebaEts);
 	}
 
 }
